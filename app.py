2b2b0992635ca27af6894fa79b1815fb88431d80
plasmotron <plasmotron@sanger.ac.uk>
2018-06-14 10:33:22 +0100

    Listen on all ports

--- app.py
+++ app.py
@@ -954,3 +954,7 @@ def addPlateForm():
     return render_template('newPlateForm.html');
 
 atexit.register(cleanup)   
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0',port=5000)
